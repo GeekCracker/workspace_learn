7981f7de758f269282e40b686c2377669234fe00
zhujunliang <1127766234@qq.com>
2019-07-31 11:09:49 +0800

    提交

--- test-maven/src/main/java/com/day20190730/test2/Test1.java
+++ test-maven/src/main/java/com/day20190730/test2/Test1.java
@@ -2,7 +2,40 @@ package com.day20190730.test2;
 
 public class Test1 {
 
+	private static Integer count = 0;
+
+	private static Object obj = new Object();
+
 	public static void main(String[] args) {
-		
+
+		Thread thread1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i = 0;i<1000;i++){
+					synchronized (obj){
+						count++;
+					}
+				}
+			}
+		});
+		Thread thread2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i = 0;i<1000;i++){
+					synchronized (obj){
+						count++;
+					}
+				}
+			}
+		});
+		thread1.start();
+		thread2.start();
+
+		try {
+			Thread.sleep(2);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(count);
 	}
 }
---
+++ test-maven/src/main/java/com/day20190731/test1/Test.java
@@ -0,0 +1,10 @@
+package com.day20190731.test1;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
