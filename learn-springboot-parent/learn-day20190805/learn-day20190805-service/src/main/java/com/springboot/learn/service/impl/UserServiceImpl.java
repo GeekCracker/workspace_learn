49837743cd25d26c5c1a055403e9406312394866
zhujunliang <1127766234@qq.com>
2019-08-05 20:21:19 +0800

    更新learn工程

---
+++ learn-springboot-parent/learn-day20190805/learn-day20190805-service/src/main/java/com/springboot/learn/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.springboot.learn.service.impl;
+
+import com.springboot.learn.domain.User;
+import com.springboot.learn.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class UserServiceImpl extends BaseServiceImpl<User> implements UserService {
+
+    @Override
+    public User queryById(String id) {
+        User user = new User();
+        user.setId(id);
+        user.setUserName("张三"+id);
+        user.setPassword("123456");
+        user.setUserAge(12+"");
+        Date date = new Date();
+        user.setCreatedTime(date);
+        user.setUpdatedTime(date);
+        return user;
+    }
+}
