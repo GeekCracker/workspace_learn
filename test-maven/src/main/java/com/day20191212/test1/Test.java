abbbcd861ab4bc47de9b4a93de4735a9afc11994
zhujunliang <1127766234@qq.com>
2019-12-30 10:01:37 +0800

    更新

--- test-maven/src/main/java/com/day20191212/test1/Test.java
+++ test-maven/src/main/java/com/day20191212/test1/Test.java
@@ -1,12 +1,20 @@
 package com.day20191212.test1;
 
-import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Test {
 
     public static void main(String[] args) {
-        String str = "%tF %tT";
-        System.out.println(String.format(str,new Date(),new Date()));
+        Set<Integer> set1 = new LinkedHashSet<Integer>();
+        set1.add(200);
+        set1.add(233);
+
+        Set<Integer> set2 = new LinkedHashSet<>();
+        set2.add(400);
+        set2.add(300);
+        set1.retainAll(set2);
+        System.out.println(set1);
 
     }
 }
